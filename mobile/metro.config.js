b64f1f191b09792ecb301e61f15f879bcab51c9f
vedant__ <278404454+vedaant7-Dev@users.noreply.github.com>
2026-05-25 20:37:51 +0000

    Optimize Android production build config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,17 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = true;
-
-config.transformer = {
-  ...config.transformer,
-  minifierConfig: {
-    compress: {
-      drop_console: true,
-      reduce_funcs: false,
-    },
-    mangle: true,
-  },
-};
-
 module.exports = config;
