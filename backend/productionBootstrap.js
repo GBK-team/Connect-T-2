092d6dbb7b9e1821176a10316071d7ccf4d50a34
vedaant-07 <se7enfits.07@gmail.com>
2026-07-23 15:44:45 +0530

    Load secure alert delivery before legacy routes

--- backend/productionBootstrap.js
+++ backend/productionBootstrap.js
@@ -7,6 +7,7 @@
 const PRODUCTION_PATCHES = [
   "./otpProductionPatch.js",
   "./utilityStatusPatch.js",
+  "./alertDeliveryPatch.js",
   "./jobPortalSessionRecoveryPatch.js",
   "./jobPortalRoleGovernancePatch.js",
   "./jobPortalAuthPatch.js",
